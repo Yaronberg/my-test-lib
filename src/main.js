c29fdad4661d12dd121b6671e05475af374b7aff
Yaroslav Malyi <ymalyi@apextech.com.ua>
2021-10-12 23:55:36 +0300

    connect script to publish on npm

--- src/main.js
+++ src/main.js
@@ -2,3 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
+
+import HelloWorld from './components/HelloWorld.vue';
+
+HelloWorld.install = function (Vue) {
+  Vue.component(HelloWorld.name, HelloWorld);
+};
+
+console.log(HelloWorld);
+console.log(HelloWorld.name);
+export default HelloWorld;
